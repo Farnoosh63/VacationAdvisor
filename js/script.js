d6e76e6e8ab5ac08e39aeb47c6c672e97db97175
Farnoosh Johnson <farnoosh@psu.ds.pdx.edu>
2016-02-02 15:16:56 -0800

    adjust the indentation in file index.html

--- js/script.js
+++ js/script.js
@@ -11,12 +11,14 @@ $(document).ready(function () {
       var budgetInput = $("select#Budget").val();
       var ageInput = $("select#Age").val();
 
-
-
       if (ageInput === "Below 18 years"){
         $(".error").show();
       }else if ((whereInput === "1") && (whatInput === "1") &&
       (budgetInput === "1")){
+       $(".nature").show();
+      }else if ((whereInput === "1") && (whatInput === "1") &&
+      (budgetInput === "2" || "3")){
+        alert("Your budget is too high for backpacking adventure, we offer you low budget ideas, enjoy the rest of your budget with something else.");
        $(".nature").show();
      }else if ((whereInput === "2") && (whatInput === "2") &&
      (budgetInput === "2")) {
@@ -25,13 +27,13 @@ $(document).ready(function () {
      (budgetInput === "2")) {
        $(".high_history").show();
      }else if ((whereInput === "3") && (whatInput === "3") &&
-     (budgetInput === "2")) {
+     (budgetInput === "3")) {
        $(".history").show();
      }else if ((whereInput === "3") && (whatInput === "3"||"2") && (budgetInput === "1")) {
        alert("Your budget is low!!!")
        $(".error").show();
      }else if ((whereInput === "2") && (whatInput === "2") && (budgetInput === "3")) {
-       alert("Your budget is low!!!")
+       alert("Your budget is low for Spa treatment and resort!!!")
        $(".error").show();
      }else if ((whereInput === "1") && (whatInput === "2") &&
      (budgetInput === "2" || "3")) {
@@ -49,15 +51,18 @@ $(document).ready(function () {
      (budgetInput === "2" || "3")) {
        alert("We cannot provide you any options with combination of Beach & Musuem, Food and culture !!!")
        $(".error").show();
+      }else if ((whereInput === "3") && (whatInput === "1") &&
+     (budgetInput === "2" || "3")) {
+       alert("We cannot provide you any options with combination of history and adventure !!!")
+       $(".error").show();
+      }else if ((whereInput === "3") && (whatInput === "2") &&
+     (budgetInput === "2" || "3")) {
+       alert("We cannot provide you any options with combination of history and Relax & Spa !!!")
+       $(".error").show();
      }else {
        alert("Incorrect input.")
        $(".error").show();
      }
-
-    // }); //end of hide result
-
-  // }); //end of click confirm
-
-  event.preventDefault();
-});  //end of submit function
+    event.preventDefault();
+  });  //end of submit function
 }); //end of document ready
